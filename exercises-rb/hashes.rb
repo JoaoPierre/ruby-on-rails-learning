52806a5f6506e6ad86e0ebd76b7f8feaa6deba17
JoaoPierre <jppierre90@gmail.com>
2023-09-13 21:09:48 -0300

    Improving hashes skills

---
+++ exercises-rb/hashes.rb
@@ -0,0 +1,13 @@
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+keys_inside = [:email, :adress, :number]
+
+puts contacts.each_with_index {|(name,hash), i| keys_inside.each {|key| hash[key]= contact_data[i]}}
+
+
+
+
+
