7c771a6b6c2608f04cbe52e0e9b4c9cc26b63f5f
JoaoPierre <jppierre90@gmail.com>
2023-09-19 12:14:36 -0300

    solve caesar ciphers exercise

--- exercises-rb/caesar_cyphers.rb
+++ exercises-rb/caesar_cyphers.rb
@@ -1,6 +1,14 @@
-def caesar_cipher(str)
-  
+def caesar_cipher(str, shift)
+  result = ""
 
-
-
+  str.each.char do |char| 
+    base = char.ord < 91 ? 65 : 97
+    if char.ord.between(65,91) || char.ord.between(97,122)
+      rotation = (((char.ord-base)+shift)%26) + base  
+      result += rotation.char()
+    else 
+      result += char 
+    end 
+  end                                                                                      
+  result
 end 
