968c27a045115fa9faa33d3b7026d4b81e89e653
JoaoPierre <jppierre90@gmail.com>
2023-09-27 17:54:26 -0300

    Improve Luhn class

--- exercises-rb/valid_card_number_class.rb
+++ exercises-rb/valid_card_number_class.rb
@@ -1,21 +1,20 @@
 class Luhn
-  def self.valid?(str)   
-    stripped_str = str.gsub(" ","")
-
-    return false if stripped_str.match?(/[^0-9\s]/) || stripped_str.length <= 1
-
-    reversed_str = stripped_str.reverse.chars.map(&:to_i)    
-
-    arr_double_digits = reversed_str.each_with_index.map do |elem, i|
-      puts "o elem #{elem} tem index #{i}" #debbguing 
-       i.odd? ? elem != 9 ?(elem * 2) % 9 : 9 : elem    
-    end 
-    
-    sum = arr_double_digits.sum
-
-    puts sum
-
-    sum % 10 == 0 ? true : false 
+  def self.valid?(identification)  #IDENTIFICATION SHOULD RECEIVE A STRING OF NUMBERS
+    stripped_identification = identification.gsub(" ","")
+   
+    return false if stripped_identification.match?(/[^0-9]/) || stripped_identification.length <= 1
+   
+    reversed_identification = stripped_identification.reverse.chars.map(&:to_i)
+   
+    reversed_identification.each_with_index.map do |digit, i|
+        i.odd? ?
+          digit.equal?(9)?
+            9 :
+            digit * 2 % 9:
+        digit
+    end
+      .sum % 10 == 0
+ 
   end
 end
 
