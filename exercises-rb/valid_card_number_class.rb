d89e5a0e5a9cbb45003774bc664398d5b8483557
JoaoPierre <jppierre90@gmail.com>
2023-09-28 10:30:39 -0300

    Final Luhn class

--- exercises-rb/valid_card_number_class.rb
+++ exercises-rb/valid_card_number_class.rb
@@ -1,11 +1,26 @@
 class Luhn
-  def self.valid?(identification)  #IDENTIFICATION SHOULD RECEIVE A STRING OF NUMBERS
-    stripped_identification = identification.gsub(" ","")
-   
-    return false if stripped_identification.match?(/[^0-9]/) || stripped_identification.length <= 1
-   
-    reversed_identification = stripped_identification.reverse.chars.map(&:to_i)
-   
+  def self.valid?(identification)
+    new(identification).valid?
+  end
+  
+  def valid?
+   return false if id_is_short? || non_numerics?
+   check_sum.equal?(0)
+  end
+
+  private
+
+  attr_reader :identification
+
+  def initialize(identification)
+    @identification = identification
+  end
+
+  def reversed_identification
+    identification.gsub(" ","").reverse.chars.map(&:to_i)
+  end
+
+  def get_digits_doubled
     reversed_identification.each_with_index.map do |digit, i|
         i.odd? ?
           digit.equal?(9)?
@@ -13,8 +28,22 @@ class Luhn
             digit * 2 % 9:
         digit
     end
-      .sum % 10 == 0
- 
+  end
+
+  def sum_of_digits
+    get_digits_doubled.sum
+  end
+
+  def check_sum
+    sum_of_digits % 10
+  end
+  
+  def id_is_short?
+    reversed_identification.length <= 1
+  end
+
+  def non_numerics?
+    identification.match?(/[^0-9\s]/)
   end
 end
 
