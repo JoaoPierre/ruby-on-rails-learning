8b7bb18f3c95f6b1ae88d997b30c97efa3d1e873
JoaoPierre <jppierre90@gmail.com>
2023-09-29 18:59:47 -0300

    Add cinema class

---
+++ exercises-rb/cinema_class.rb
@@ -0,0 +1,26 @@
+class NotMovieClubMemberError < RuntimeError
+end
+
+class Moviegoer
+
+  attr_reader :age, :member
+  
+  def initialize(age, member: false)
+    @age = age
+    @member = member
+  end
+
+  def ticket_price
+    age >= 60 ? 10.00 : 15.00
+  end
+
+  def watch_scary_movie?
+    age >= 18
+  end
+
+  # Popcorn is 🍿
+  def claim_free_popcorn!
+    raise NotMovieClubMemberError unless member
+    "🍿"
+  end
+end
