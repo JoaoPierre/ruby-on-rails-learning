d349316261790c26f7739fb0ddbf6fe1359e10fe
JoaoPierre <jppierre90@gmail.com>
2023-10-02 16:03:44 -0300

    Create new action and view

--- projects-rails/blog1/config/routes.rb
+++ projects-rails/blog1/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
   # Defines the root path route ("/")
-  # root "articles#index"
+
  root "blog_posts#index"
 end
