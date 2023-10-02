5599da4e73eb300802e832e59c25aac9981eb627
JoaoPierre <jppierre90@gmail.com>
2023-10-02 17:46:18 -0300

    Add blog post destroy action and refactors controllers and routes

--- projects-rails/blog1/app/controllers/blog_posts_controller.rb
+++ projects-rails/blog1/app/controllers/blog_posts_controller.rb
@@ -1,12 +1,11 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, except: [:new, :index, :create]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
   end
 
   def new
@@ -21,11 +20,9 @@ class BlogPostsController < ApplicationController
     end
   end
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def update
-      @blog_post = BlogPost.find(params[:id])
       if @blog_post.update(blog_post_params)
         redirect_to @blog_post
       else
@@ -33,6 +30,10 @@ class BlogPostsController < ApplicationController
       end
   end
 
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path
+  end
 
 
 private
@@ -40,4 +41,11 @@ private
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
   end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
 end
--- projects-rails/blog1/config/routes.rb
+++ projects-rails/blog1/config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  get "blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
-  # Defines the root path route ("/")
+  resources :blog_posts
 
  root "blog_posts#index"
 end
