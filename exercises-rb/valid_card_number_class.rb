616841f9bdc77c325230ba6cfbd60a1f142f321d
JoaoPierre <jppierre90@gmail.com>
2023-10-02 21:49:20 -0300

    Improve luhn identification class

--- exercises-rb/valid_card_number_class.rb
+++ exercises-rb/valid_card_number_class.rb
@@ -1,24 +1,36 @@
 class Luhn
+
   def self.valid?(identification)
     new(identification).valid?
   end
+
   def valid?
    return false if id_is_short? || non_numerics?
    check_sum.equal?(0)
   end
+
   private
+
   attr_reader :identification
+
   def id_is_short?
-    identification.length <= 1
+    digits.length <= 1
   end
+
   def non_numerics?
     identification.to_s.match?(/[^0-9\s]/)
   end
+
   def initialize(identification)
-    @identification = identification.gsub(" ","")
+    @identification = identification
   end
+
+  def digits
+    identification.reverse.scan(/\d/).map(&:to_i)
+  end
+
   def digits_doubled
-    identification.reverse.chars.map(&:to_i).each_with_index.map do |digit, i|
+    digits.each_with_index.map do |digit, i|
         i.odd? ?
           digit.equal?(9)?
             9 :
@@ -26,15 +38,15 @@ class Luhn
         digit
     end
   end
+
   def sum_of_digits
     digits_doubled.sum
   end
+
   def check_sum
     sum_of_digits % 10
   end
+
 end
 
-
 Luhn.valid?("091")
-      
-  
