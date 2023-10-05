91ba4e54d91580dc1dc560dfdabf3aaa400fb741
JoaoPierre <jppierre90@gmail.com>
2023-10-05 13:06:24 -0300

    Improve luhn class

--- exercises-rb/valid_card_number_class.rb
+++ exercises-rb/valid_card_number_class.rb
@@ -1,34 +1,22 @@
 class Luhn
-  REGULAR_EXPRESSION = {any_non_numerics?: /[^0-9\s]/}
-  def self.valid?(identification)
-    new(identification).valid?
-  end
-
-  def valid?
-   return false if id_is_short? || non_numerics?
-   check_sum.equal?(0)
-  end
-
+  REGULAR_EXPRESSION = {
+    any_non_numerics?: /[^0-9\s]/,
+    digit: /\d/}
   private
 
   attr_reader :identification
-
+  def initialize(identification)
+    @identification = identification.reverse
+  end
   def id_is_short?
     digits.length <= 1
   end
-
   def non_numerics?
-    identification.to_s.match?(REGULAR_EXPRESSION[:any_non_numerics?])
+    identification.match?(REGULAR_EXPRESSION[:any_non_numerics?])
   end
-
-  def initialize(identification)
-    @identification = identification
-  end
-
   def digits
-    identification.reverse.scan(/\d/).map(&:to_i)
+    identification.scan(REGULAR_EXPRESSION[:digit]).map(&:to_i)
   end
-
   def digits_doubled
     digits.each_with_index.map do |digit, i|
         i.odd? ?
@@ -38,15 +26,18 @@ class Luhn
         digit
     end
   end
-
   def sum_of_digits
     digits_doubled.sum
   end
-
   def check_sum
     sum_of_digits % 10
   end
-
+  public
+  def self.valid?(identification)
+    new(identification).valid?
+  end
+  def valid?
+   return false if id_is_short? || non_numerics?
+   check_sum.equal?(0)
+  end
 end
-
-Luhn.valid?("091")
