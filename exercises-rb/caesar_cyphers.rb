6d1cf3fc3e314de295f50bbfef37a4702ed31899
JoaoPierre <jppierre90@gmail.com>
2023-10-24 16:37:46 -0300

    Improve caesar cyphers class

--- exercises-rb/caesar_cyphers.rb
+++ exercises-rb/caesar_cyphers.rb
@@ -1,14 +1,31 @@
-def caesar_cipher(str, shift)
-  result = ""
+class RotationalCipher
 
-  str.each.char do |char| 
-    base = char.ord < 91 ? 65 : 97
-    if char.ord.between(65,91) || char.ord.between(97,122)
-      rotation = (((char.ord-base)+shift)%26) + base  
-      result += rotation.char()
-    else 
-      result += char 
-    end 
-  end                                                                                      
-  result
-end 
+  def self.rotate(string, key)
+    new(string, key).rotate
+  end
+
+  private
+
+  attr_reader :string, :key
+
+  def initialize(string , key)
+    @string = string
+    @key = key
+  end
+
+  public
+
+  def rotate
+    result = ""
+    string.chars.each do |char|
+      base = char.ord < 91 ? 65 : 97
+      if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+        rotate_letter = ((char.ord + key - base) %26)+ base
+        result << rotate_letter.chr
+      else
+        result << char
+      end
+    end
+    result
+  end
+end
