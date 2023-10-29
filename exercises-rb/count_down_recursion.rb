788f71dee911c2cff200e01fe41c3366d834e23c
JoaoPierre <jppierre90@gmail.com>
2023-10-29 19:28:59 -0300

    fix count down function

--- exercises-rb/count_down_recursion.rb
+++ exercises-rb/count_down_recursion.rb
@@ -1,11 +1,11 @@
 def count_down(number)
   puts number
-  if number == 0 
+  if number == 0
     number
-  else 
-    count_down(number-1)
+  else
+    count_down(number - 1)
   end
-end 
+end
 
 count_down(10)
 count_down(20)
