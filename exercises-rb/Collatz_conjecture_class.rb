8325cd7a8e6c3f7b54ac1051810d93df42180483
JoaoPierre <jppierre90@gmail.com>
2023-10-30 15:50:20 -0300

    Add recursive collatz conjecture class

---
+++ exercises-rb/Collatz_conjecture_class.rb
@@ -0,0 +1,33 @@
+class CollatzConjecture
+
+  def self.steps(number)
+    new(number).steps
+  end
+
+  private
+
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def argument_error
+    raise ArgumentError unless number.positive?
+  end
+
+  public
+
+  def steps
+    argument_error
+
+    return 0 if number == 1
+
+    if number.even?
+       @number /= 2
+    else
+      @number = number * 3 + 1
+    end
+    1 + steps
+  end
+end
