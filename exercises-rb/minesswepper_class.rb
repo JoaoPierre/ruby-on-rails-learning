89a55420be0d1f0a5eb9aae706f88e3e86fff68b
JoaoPierre <jppierre90@gmail.com>
2023-10-30 15:48:44 -0300

    start mineswepper class

---
+++ exercises-rb/minesswepper_class.rb
@@ -0,0 +1,22 @@
+class Minesweeper
+  def self.annotate(input)
+    new(input).annotate
+  end
+
+  attr_accessor :input
+
+  def initialize input
+    @input = input
+  end
+
+  def annotate
+    input.map_with_index do |line, row_index|
+      line.chars.map_with_index do |elem ,column|
+
+      end
+    end
+
+  end
+
+end
+Minesweeper.annotate(["   ", " * ", "   "])
