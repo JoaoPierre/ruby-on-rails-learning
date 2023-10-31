f7375e7fd4a0f8d63599ba7fbe67db8e6aa24446
JoaoPierre <jppierre90@gmail.com>
2023-10-31 18:38:01 -0300

    Add resistor_color class

---
+++ exercises-rb/resistor_color.rb
@@ -0,0 +1,16 @@
+class ResistorColor
+  COLORS = %w[black brown red orange yellow green blue violet grey white]
+  private
+
+  def initialize(color)
+    @color = color
+  end
+  attr_reader :color
+  def self.color_code(color)
+    new(color).color_code
+  end
+  public
+  def color_code
+    COLORS.index(color)
+  end
+end
