6bcfc570cad85550ef5bb9f38e864c7ca88c0af3
JoaoPierre <jppierre90@gmail.com>
2023-11-07 23:40:48 -0300

    Simplify isbn class

--- exercises-rb/isbn_verify.rb
+++ exercises-rb/isbn_verify.rb
@@ -1,55 +1,17 @@
 class IsbnVerifier
   X_VALUE = "10"
-
-  REGULAR_EXPRESSION = {valid_characters: /\d|[X]/,}
+  REGULAR_EXPRESSION = {
+    valid_expression: /^\d-?\d{3}-?\d{5}-?[\dX]$/,
+    digits: /\d|X/}
   RE = REGULAR_EXPRESSION
   private_constant :RE
 
-  def self.valid?(identification)
-    new(identification).valid?
-  end
-
-  def initialize(identification)
-    @identification = identification.scan(RE[:valid_characters])
-  end
-
-  attr_reader :identification
-
-  def valid?
-   return false if length_issue? || x_position_restriction
-   check_sum.equal?(0)
-  end
-
-  def have_x?
-    if identification.include?("X")
-      identification.pop
-      p identification.push(X_VALUE)
-    else
-      identification
+  def self.valid?(id)
+    if id.match?(RE[:valid_expression])
+      digits = id.scan(RE[:digits])
+      digits[-1] = X_VALUE if digits.last == "X"
+      total = digits.reverse.map(&:to_i).map.with_index{|digit ,i| (i + 1) * digit}.sum
+      (total % 11).equal?(0)
     end
-
-  end
-
-  def x_position_restriction
-    identification.include?("X") ?
-      identification.last != "X" ?
-        true : false
-      : false
-  end
-
-  def length_issue?
-    identification.length != 10
-  end
-
-  def isbn_sum
-    sum = 0
-    have_x?.map(&:to_i).reverse.each_with_index{|num , index| sum += num * (index + 1)}
-    sum
-  end
-
-  def check_sum
-    isbn_sum % 11
   end
 end
-
-IsbnVerifier.valid?("3-598-21507-X")
