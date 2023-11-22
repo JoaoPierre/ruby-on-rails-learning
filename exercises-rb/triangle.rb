c35e457d7f03b546c897d3111cda9dc04423ede5
JoaoPierre <jppierre90@gmail.com>
2023-11-22 10:50:25 -0300

    Improve  triangle class

--- exercises-rb/triangle.rb
+++ exercises-rb/triangle.rb
@@ -1,37 +1,35 @@
 class Triangle
-  public
+  attr_accessor :sides
+
+  def initialize(sides)
+    raise ArgumentError unless sides.length == 3
+    @sides = sides.sort
+  end
 
   def equilateral?
     return false unless fits_triangle_rule?
-    first_side == second_side && second_side == third_side
+    sides.uniq.length == 1
   end
+
   def isosceles?
     return false unless fits_triangle_rule?
-    case
-    when sides.select{|side| side == first_side}.length.equal?(2) then true
-    when sides.select{|side| side == second_side}.length.equal?(2) then true
-    when sides.select{|side| side == third_side}.length.equal?(2) then true
-    when sides.select{|side| side == first_side}.length.equal?(3) then true
-    else false
-    end
+    sides.uniq.length <= 2
   end
+
   def scalene?
     return false unless fits_triangle_rule?
     sides.uniq.length.equal?(3)
   end
-  private
-  def initialize(sides)
-    @first_side = sides[0]
-    @second_side = sides[1]
-    @third_side = sides[2]
-    @sides = sides
-  end
-  attr_reader :first_side, :second_side, :third_side, :sides
+
   def valid_sides?
     sides.all?{|side| side > 0}
   end
+
   def fits_triangle_rule?
     return false unless valid_sides?
-    first_side+second_side >= third_side && first_side+third_side >= second_side && third_side+second_side >= first_side
+    sides[0] + sides[1] >= sides[2]
   end
 end
+
+triangle = Triangle.new([2,2,2])
+triangle.isosceles?
