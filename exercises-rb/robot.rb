1857af722d0ad5d21a0f5124b3feb258af00edf2
JoaoPierre <jppierre90@gmail.com>
2024-01-31 23:15:23 -0300

    Update robot class

--- exercises-rb/robot.rb
+++ exercises-rb/robot.rb
@@ -1,25 +1,73 @@
 class Robot
-  POSSIBLE_DIRECTIONS = %i[north, west, south, east]
+  POSSIBLE_DIRECTIONS = %i[north east south west]
+  PD = POSSIBLE_DIRECTIONS
+  private_constant :PD
 
-  attr_accessor :current_facing, :x, :y
-
-  def initialize(current_facing = nil, x = 0, y = 0) #x e y are catesian coordinates
+  def initialize(current_facing = nil, x = 0,y = 0) #x e y are catesian coordinates
     @x = x
     @y = y
     @current_facing = current_facing
+    @counter = 0
   end
 
   def orient(direction)
-    raise ArgumentError, "Direction does not accepts #{direction}" unless POSSIBLE_DIRECTIONS.include?(direction)
-    current_facing= direction
-    puts current_facing
+    raise ArgumentError.new "argument issue" unless POSSIBLE_DIRECTIONS.include?(direction)
+    @current_facing = direction
   end
 
   def bearing
-    current_facing
+    @current_facing
   end
 
+  def turn_right
+    @counter = PD.index(@current_facing)
+    @current_facing = PD[(@counter+1)% 4]
+  end
+
+  def turn_left
+     @counter = PD.index(@current_facing)
+     @current_facing = PD[(@counter-1)% 4]
+  end
+
+  def at(x , y)
+    @x = x
+    @y = y
+    [@x, @y]
+  end
+
+  def coordinates
+    [@x , @y]
+  end
+
+  def advance
+    case @current_facing
+      when @current_facing = :north then @y += 1
+      when @current_facing = :east then  @x += 1
+      when @current_facing = :south then @y -= 1
+      when @current_facing = :west then  @x -= 1
+    end
+  end
 end
 
-robot = Robot.new
-robot.orient(:south)
+
+class Simulator < Robot
+
+  INTRUCTIONS = {"A"=> :advance, "R" => :turn_right, "L" => :turn_left}
+
+  def instructions(instructions)
+    sequence= []
+    instructions.split("").each do |instruction|
+      sequence.push(INTRUCTIONS[instruction])
+    end
+    sequence
+  end
+
+  def place(robot, x:, y:, direction:)
+    orient(:direction)
+    at(:x,:y)
+  end
+
+  def evaluate(robot , instructions)
+
+  end
+end
