581df5bbbcb75c4f206b53fff8f18d8c2d13556d
JoaoPierre <jppierre90@gmail.com>
2024-03-18 17:13:28 -0300

    Create student class

---
+++ complete_ruby_on_rails/student.rb
@@ -0,0 +1,21 @@
+require_relative 'crud'
+
+class Student
+  include Crud
+  attr_accessor :first_name,:last_name, :email, :username, :password
+
+  def initialize(first_name, last_name, username, password, email)
+    @email = email
+    @first_name = first_name
+    @last_name = last_name
+    @username = username
+    @password = password
+  end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}"
+  end
+end
+
+joao = Student.new("joao", "pierre", "pipi", "20344996@", "jppierre90@gmail.com")
+p joao
