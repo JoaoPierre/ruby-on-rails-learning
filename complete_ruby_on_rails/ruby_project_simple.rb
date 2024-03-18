84d2c0471905b24748342a2dc088b687f967e173
JoaoPierre <jppierre90@gmail.com>
2024-03-18 17:14:55 -0300

    Use module mixins to work with a diferent class

---
+++ complete_ruby_on_rails/ruby_project_simple.rb
@@ -0,0 +1,14 @@
+require_relative 'crud'
+
+#$LOAD_PATH << "."
+#require 'crud'
+
+users = [
+      { username: "mashrur", password: "password1" },
+      { username: "jack", password: "password2" },
+      { username: "arya", password: "password3" },
+      { username: "jonshow", password: "password4" },
+      { username: "heisenberg", password: "password5" }
+        ]
+
+hashed_users = Crud.create_secure_users(users)
