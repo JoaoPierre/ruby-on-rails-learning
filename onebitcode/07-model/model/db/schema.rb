9a278f5002b12e4f95c415d69c3ebcb51113daed
JoaoPierre <jppierre90@gmail.com>
2024-04-06 10:20:22 -0300

    Generate models

--- onebitcode/07-model/model/db/schema.rb
+++ onebitcode/07-model/model/db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_174959) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_06_131932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_174959) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "authors"
 end
